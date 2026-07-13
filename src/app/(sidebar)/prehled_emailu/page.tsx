e9da2013da29b038ccac12fa1e4c54a5038ee715
thomicz <tomassdvorakk@gmail.com>
2026-07-13 11:48:12 +0200

    added print

--- src/app/(sidebar)/prehled_emailu/page.tsx
+++ src/app/(sidebar)/prehled_emailu/page.tsx
@@ -1,11 +1,14 @@
 import PageHeader from '@/components/custom/layout/page-header'
 
+import { PageActions } from './page-actions'
 import { PrehledEmailu } from './prehled_emailu'
 
 export default function Page() {
     return (
         <>
-            <PageHeader breadcrumbs={[{ label: 'Přehled e-mailů' }]} />
+            <PageHeader breadcrumbs={[{ label: 'Přehled e-mailů' }]}>
+                <PageActions />
+            </PageHeader>
             <div className="flex w-full justify-center p-3">
                 <PrehledEmailu />
             </div>
---
+++ src/app/print/prehled_emailu/page.tsx
@@ -0,0 +1,13 @@
+import { PrehledEmailu } from '@/app/(sidebar)/prehled_emailu/prehled_emailu'
+
+import { PrintShell, type PrintPageSettings } from '../components/print-shell'
+
+const printPageSettings: PrintPageSettings = {
+    title: 'Přehled e-mailů',
+    description: 'Doručeno, otevřeno, kliknuto a doručitelnost po měsících.',
+    body: <PrehledEmailu />,
+}
+
+export default function PrintPrehledEmailuPage() {
+    return <PrintShell {...printPageSettings} />
+}
