6038a620045596caa259247f7dcc0da12afcd43e
thomicz <tomassdvorakk@gmail.com>
2026-07-14 09:22:59 +0200

    removed other stuff from branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,12 @@ const pages: {
         status: 'progress',
         by: 'Denis',
     },
+    {
+        name: 'Přehled popup',
+        path: '/prehled_popup',
+        status: 'progress',
+        by: 'Tomas',
+    },
 ]
 
 export default function Page() {
