78c86cc5d66a3cea8c092dc169d66d695a712e24
thomicz <tomassdvorakk@gmail.com>
2026-07-14 09:07:59 +0200

    updated dasboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,12 @@ const pages: {
         status: 'progress',
         by: 'Denis',
     },
+    {
+        name: 'Stats Page',
+        path: '/stats',
+        status: 'progress',
+        by: 'Tomas',
+    },
 ]
 
 export default function Page() {
